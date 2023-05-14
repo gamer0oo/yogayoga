f226b0208585f3d1dcaacc63b4fb35c164b876cf
sarten <steffi.ryser.t@gmail.com>
2023-05-14 01:53:07 -0400

    Agregar api de yoga, cambios en sucursales y contacto

--- assets/js/contacto.js
+++ assets/js/contacto.js
@@ -2,8 +2,8 @@ let rut = document.getElementById("rutInput")
 let nombre = document.getElementById("nameInput");
 let apellido = document.getElementById("apellInput");
 let boton = document.getElementById("enviar");
-let telefono = document.getElementById("telInput");  
-let correo = document.getElementById("correoInput")  
+let telefono = document.getElementById("telInput");
+let correo = document.getElementById("correoInput")
 let direccion = document.getElementById("direccInput")
 let contraseña = document.getElementById("pswInput")
 let contraseña2 = document.getElementById("psw2Input")
@@ -11,43 +11,43 @@ let asunto = document.getElementById("asunInput")
 
 boton.addEventListener("click", validarFormulario)
 
-function validarFormulario (e){
+function validarFormulario(e) {
     e.preventDefault();
 
     let hayErrores = false;
 
-    if(rut.value.length < 8 || rut.value.length > 9){
+    if (rut.value.length < 8 || rut.value.length > 9) {
         $("#error8").css("display", "block");
         hayErrores = true;
 
     } else {
         $("#error8").css("display", "none");
     }
-    if(contraseña.value == '' || contraseña.value.length < 5){
+    if (contraseña.value == '' || contraseña.value.length < 5) {
         $("#error6").css("display", "block");
         hayErrores = true;
-        
+
     } else {
         $("#error6").css("display", "none");
     }
-    if(contraseña2.value !== contraseña.value){
+    if (contraseña2.value !== contraseña.value) {
         $("#error7").css("display", "block");
         hayErrores = true;
-    
+
     } else {
-    $("#error7").css("display", "none");
+        $("#error7").css("display", "none");
 
     }
 
 
-    if(nombre.value == ''){
+    if (nombre.value == '') {
         $("#error").css("display", "block");
         hayErrores = true;
     } else {
         $("#error").css("display", "none");
     }
 
-    if (apellido.value == ''){
+    if (apellido.value == '') {
         $("#error2").css("display", "block");
         hayErrores = true;
     } else {
@@ -68,13 +68,13 @@ function validarFormulario (e){
         $("#error4").css("display", "none");
     }
 
-    if (direccion.value == ''){
+    if (direccion.value == '') {
         $("#error5").css("display", "block");
         hayErrores = true;
     } else {
         $("#error5").css("display", "none");
     }
-    if (asunto.value == ''){
+    if (asunto.value == '') {
         $("#error9").css("display", "block");
         hayErrores = true;
     } else {
@@ -86,56 +86,72 @@ function validarFormulario (e){
     }
 
     function limpiarCampos() {
-        rut.value="";
-        contraseña.value="";
-        contraseña2.value="";
+        rut.value = "";
+        contraseña.value = "";
+        contraseña2.value = "";
         nombre.value = "";
         apellido.value = "";
         telefono.value = "";
         correo.value = "";
         direccion.value = "";
-        asunto.value="";
+        asunto.value = "";
     }
 
     alert("Formulario enviado correctamente");
     limpiarCampos();
 };
-rut.addEventListener("input", function() {
+rut.addEventListener("input", function () {
     $("#error8").css("display", "none");
 });
 
-contraseña.addEventListener("input", function() {
+contraseña.addEventListener("input", function () {
     $("#error6").css("display", "none");
 });
-contraseña2.addEventListener("input", function() {
+contraseña2.addEventListener("input", function () {
     $("#error9").css("display", "none");
 });
 
-nombre.addEventListener("input", function() {
+nombre.addEventListener("input", function () {
     $("#error").css("display", "none");
 });
 
-apellido.addEventListener("input", function() {
+apellido.addEventListener("input", function () {
     $("#error2").css("display", "none");
 });
 
-telefono.addEventListener("input", function() {
+telefono.addEventListener("input", function () {
     $("#error3").css("display", "none");
 });
 
-correo.addEventListener("input", function() {
+correo.addEventListener("input", function () {
     $("#error4").css("display", "none");
 });
 
 
-direccion.addEventListener("input", function() {
+direccion.addEventListener("input", function () {
     $("#error5").css("display", "none");
 });
 
-asunto.addEventListener("input",function() {
+asunto.addEventListener("input", function () {
     $("#error9").css("display", "none");
 });
 
-contraseña2.addEventListener("input",function() {
+contraseña2.addEventListener("input", function () {
     $("#error7").css("display", "none");
 });
+
+function password() {
+    let input = document.getElementById("password");
+
+    if (input.type == "password") {
+        input.type = "text";
+        document.getElementById("mostrar_pass").style.display = "none";
+        document.getElementById("ocultar_pass").style.display = "inline";
+    }
+    else {
+        input.type = "password";
+        document.getElementById("mostrar_pass").style.display = "inline";
+        document.getElementById("ocultar_pass").style.display = "none";
+    }
+
+}
---
+++ assets/js/poses.js
@@ -0,0 +1,20 @@
+const URL = "https://yoga-api-nzy4.onrender.com/v1/poses";
+let poses = [];
+
+async function obtPoses() {
+    await fetch(URL)
+        .then(res => res.json())
+        .then(json => poses.push(json))
+
+    for (let i = 0; i < 20; i++) {
+        let div = document.createElement('div');
+        div.classList.add('pose');
+        let info = `<img src=${poses[0][i].url_svg}></img> 
+                        <h3> ${poses[0][i].sanskrit_name}</h3>`;
+        div.innerHTML = info;
+        $(".poses").append(div);
+    }
+}
+
+obtPoses();
+
