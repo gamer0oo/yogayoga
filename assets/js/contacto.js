52c10347c1d5f8884296779fa350c31447025b14
sarten <steffi.ryser.t@gmail.com>
2023-05-17 01:00:44 -0400

    Nav en todas las páginas corregido

--- assets/js/contacto.js
+++ assets/js/contacto.js
@@ -206,3 +206,12 @@ function password() {
     }
 }
 
+var map
+function initMap() {
+    macc = { lat: -33.500216, lng: -70.615721 }
+    map = new google.maps.Map(document.getElementById("map"), { zoom: 17, center: macc });
+    market = new google.maps.Maker({ position: macc, map: map });
+}
+
+initMap()
+
