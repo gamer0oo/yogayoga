99d58376082d081d6cc9428b47b0df5852570522
sarten <steffi.ryser.t@gmail.com>
2023-05-17 02:02:10 -0400

    Crear Clases y ajustar nav

--- assets/js/contacto.js
+++ assets/js/contacto.js
@@ -210,7 +210,7 @@ var map
 function initMap() {
     macc = { lat: -33.500216, lng: -70.615721 }
     map = new google.maps.Map(document.getElementById("map"), { zoom: 17, center: macc });
-    market = new google.maps.Maker({ position: macc, map: map });
+    marker = new google.maps.Marker({ position: macc, map: map });
 }
 
 initMap()
