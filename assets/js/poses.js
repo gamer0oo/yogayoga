f5a385840c4d242c1a059b45ba8ed33ebbc0b286
sarten <steffi.ryser.t@gmail.com>
2023-05-16 23:22:54 -0400

    Efectos para las poses

--- assets/js/poses.js
+++ assets/js/poses.js
@@ -8,7 +8,7 @@ async function obtPoses() {
 
     for (let i = 0; i < 20; i++) {
         let div = document.createElement('div');
-        div.classList.add('pose', 'col-sm-2');
+        div.classList.add('pose', 'col-sm-2', 'mt-5');
         let info = `<img src=${poses[0][i].url_svg}></img> 
                         <h3> ${poses[0][i].sanskrit_name}</h3>
                         <p><span>Beneficios</span><br> ${poses[0][i].pose_benefits}</p>`;
