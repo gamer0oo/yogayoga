7f60433686f8cd3b0182bbf2d11150f5b113d51e
sarten <steffi.ryser.t@gmail.com>
2023-05-21 23:27:20 -0400

    Correción footer, agregar puntos y guion automaticamente a rut y limitar caracteres

--- assets/js/contacto.js
+++ assets/js/contacto.js
@@ -9,38 +9,51 @@ let contraseña = document.getElementById("pswInput");
 let contraseña2 = document.getElementById("psw2Input");
 let asunto = document.getElementById("asunInput");
 let comentarios = document.getElementById("comentInput");
+let map;
+let marker;
 
 let palabrasRestantes = document.getElementById("palabrasRestantes");
 let maxPalabras = 100;
 
+// Rellenar puntos y guion de rut automaticamente
+rut.addEventListener('input', (event) => {
+    let rut2 = event.target.value.replace(/[^0-9kK]/g, '');
+    rut2 = rut2.slice(0, 9);    // Limitar a 9 caracteres 
+    rut2 = rut2.slice(0, -1) + '-' + rut2.slice(-1);
+    rut2 = rut2.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
+
+    event.target.value = rut2;
+});
+
 function validarRut(rut) {
     rut = rut.replace(/[^0-9kK]/g, ''); // Eliminar todos los caracteres excepto números y la letra 'k' en caso de ser verificador
-  
+
     if (rut.length < 2) {
-      return false;
+        return false;
     }
-  
+
     var dv = rut.charAt(rut.length - 1); // Último dígito o letra verificadora
     var rutNumerico = parseInt(rut.slice(0, -1)); // Convertir a número el RUT sin el dígito verificador
-  
+
     if (isNaN(rutNumerico)) {
-      return false;
+        return false;
     }
-  
+
     var suma = 0;
     var factor = 2;
-  
+
     // Calcular la suma ponderada de los dígitos del RUT
     for (var i = rutNumerico.toString().length - 1; i >= 0; i--) {
-      suma += parseInt(rutNumerico.toString().charAt(i)) * factor;
-      factor = factor === 7 ? 2 : factor + 1;
+        suma += parseInt(rutNumerico.toString().charAt(i)) * factor;
+        factor = factor === 7 ? 2 : factor + 1;
     }
-  
+
     var dvEsperado = 11 - (suma % 11); // Calcular el dígito verificador esperado
     dvEsperado = dvEsperado === 11 ? 0 : dvEsperado === 10 ? 'K' : dvEsperado.toString(); // Si el dígito verificador esperado es 11, se convierte a 0, si es 10, se convierte a 'K'
-  
+
     return dv.toString().toUpperCase() === dvEsperado; // Comparar el dígito verificador ingresado con el esperado (ignorar mayúsculas/minúsculas)
-  }
+}
+
 
 boton.addEventListener("click", validarFormulario);
 
@@ -53,9 +66,9 @@ function validarFormulario(e) {
     if (!validarRut(rut.value)) {
         $("#error8").css("display", "block");
         hayErrores = true;
-      } else {
+    } else {
         $("#error8").css("display", "none");
-      }
+    }
 
     if (contraseña.value == "" || contraseña.value.length <= 7) {
         $("#error6").css("display", "block");
@@ -69,12 +82,6 @@ function validarFormulario(e) {
         hayErrores = true;
     } else {
         $("#error7").css("display", "none");
-    }
-    if (contraseña2.value == "") {
-        $("#error12").css("display", "block");
-        hayErrores = true;
-    } else {
-        $("#error12").css("display", "none");
     }
 
     if (nombre.value == "") {
@@ -227,7 +234,7 @@ function limpiarCampos() {
 }
 
 function mostrarAlertaExito() {
-    // Mostrar la alerta de éxito utilizando el modal de Bootstrap
+    // Mostrar la alerta de éxito utilizando el modal
     var modal = new bootstrap.Modal(document.getElementById('successModal'));
     modal.show();
 }
@@ -246,8 +253,7 @@ function password() {
     }
 }
 
-let map;
-let marker;
+
 
 function initMap() {
     if (navigator.geolocation) {
@@ -255,10 +261,10 @@ function initMap() {
     }
     function displayMap(position) {
         macc = { lat: position.coords.latitude, lng: position.coords.longitude }
-        map = new google.maps.Map(document.getElementById("map"), {zoom: 17, center: macc});
-        marker = new google.maps.Marker({position: macc, map: map});
+        map = new google.maps.Map(document.getElementById("map"), { zoom: 17, center: macc });
+        marker = new google.maps.Marker({ position: macc, map: map });
     }
-   
+
 }
 
 initMap()
